f68647afa1927d36fffaf718b92c306839341f36
mahmud035 <mahamudulhasan4148@gmail.com>
2022-12-02 10:23:03 +0600

    0-5: Using Link component, Image component & Head component for better SEO

--- pages/blogs/blog1.js
+++ pages/blogs/blog1.js
@@ -1,7 +1,20 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+
 const blog1 = () => {
   return (
     <div>
+      <Head>
+        <title>Blog Page</title>
+      </Head>
+
       <h1>This is a Blog Page: 1</h1>
+      <Link href="/">
+        <button>Home</button>
+      </Link>
+
+      <Image src="/nature.jpeg" width={600} height={400}></Image>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+import Head from 'next/head';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return <div className={styles.container}></div>;
+  return (
+    <div>
+      <Head>
+        <title>Home Page</title>
+      </Head>
+
+      <h1>This is Home Page</h1>
+      <Link href="/blogs/blog1">
+        <button>Blog</button>
+      </Link>
+    </div>
+  );
 }
